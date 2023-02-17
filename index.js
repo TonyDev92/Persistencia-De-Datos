be79bb806e477ee1d83489c341a4b7a0a61cb27d
TonyDev92 <tonydeveloper92@gmail.com>
2023-02-17 12:42:59 +0100

    Ejercicio 17 Javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+const nombre = "Tony";
+const apellido = "Carrasco";
+
+const developer = {
+    nombre,
+    apellido
+}
+//localStorage.getItem("developer")
+//localStorage.setItem("desarollador",JSON.stringify({developer}));
+
+// sessionStorage.setItem("Tony",developer)
+
+// document.cookie = "Finally = cookieEnd; expires=" + new Date (2023,01,17).setHours(12.39,00,0);
+
